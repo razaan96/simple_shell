338c64cadc35374553f10ececc324c3e3fa59300
razaan96 <razaan.alaa@hotmail.com>
2023-11-15 16:42:41 -0800

    making betty happy

--- getenv.c
+++ getenv.c
@@ -1,23 +1,20 @@
 #include "main.h"
 /**
- * _getsenv - Get the value of an environment variable
- * @var: Name of the environment variable
- * Return: Value of the environment variable or NULL
- */
+* _getsenv - Get the value of an environment variable
+* @var: Name of the environment variable
+* Return: Value of the environment variable or NULL
+*/
 char *_getsenv(char *var)
 {
-    extern char **environ;
-    int i, j, len;
+char **environ;
+int i, j, len;
 
-    for (i = 0; environ[i]; i++)
-    {
-        len = _strlen(var);
-        for (j = 0; j < len && environ[i][j] == var[j]; j++)
-            ;
-
-        if (j == len && environ[i][j] == '=')
-            return (environ[i] + len + 1);
-    }
-
-    return (NULL);
+for (i = 0; environ[i]; i++)
+{
+len = _strlen(var);
+for (j = 0; j < len && environ[i][j] == var[j]; j++)
+if (j == len && environ[i][j] == '=')
+return (environ[i] + len + 1);
+}
+return (NULL);
 }
